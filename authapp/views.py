0585a0461490434f0b985891d199561f46aae17b
Vansh Thakkar <vanshpthakkar@gmail.com>
2024-07-11 00:14:10 +0530

    login, company registration pages added

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login_render(request):
+    return render(request,"authapp/login.html"),
+
+def company_registration(request):
+    return render(request, "authapp/companyRegister.html")
